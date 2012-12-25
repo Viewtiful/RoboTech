f2740546c5559991553b3540c4a8b1e5e073322a
Antoine <Antoine@Antoine.home>
2012-12-25 21:48:34 +0100

    [Mise à jour] => Début de réflexion sur les boutons et comment les implémenter

---
+++ RoboTech/src/bouton/Receiver.java
@@ -0,0 +1,6 @@
+package bouton;
+
+public interface Receiver {
+
+	public abstract void change_behaviour();
+}
---
+++ RoboTech/src/bouton/Switch.java
@@ -0,0 +1,38 @@
+package bouton;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import interfaces.Drawable;
+
+public abstract class Switch implements Drawable {
+
+	Image Switch_off;
+	Image Switch_on;
+	boolean on;
+	ArrayList<Receiver> effet;
+
+	public Switch(String Switch_off, String Switch_on) {
+		this.on = false;
+		effet = new ArrayList<Receiver>();
+	}
+
+	public void add(Receiver r)
+	{
+		effet.add(r);
+	}
+	/*
+	 * Fonction qui détermine si le bouton est actionné ou pas
+	 */
+	public abstract boolean activate_Switch();
+
+	public void update(GameContainer container, StateBasedGame game, int delta)
+			throws SlickException {
+		on = activate_Switch();
+	}
+}
