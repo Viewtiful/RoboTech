4af6035d0a2dd9806cd88a78d9f3fc8b258a828d
baptiste <baptiste.haudegand@gmail.com>
2013-01-10 15:56:41 +0100

    Ennemis vert devient chausse souris! Gestion animation

--- RoboTech/src/personnages/ChauveSouris.java
+++ RoboTech/src/personnages/ChauveSouris.java
@@ -6,25 +6,41 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.XMLPackedSheet;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class EnnemisVert extends Ennemis {
+public class ChauveSouris extends Ennemis {
 	// l'image qui contient le sprite du robot
 	private Image image;
+	private XMLPackedSheet sheet;
+	private int i;
+	private int animationStep;
 
-	public EnnemisVert(float x, float y, float masse, float tailleBlockPerso,
-			Monde monde) {
+	public ChauveSouris(float x, float y, float masse, float tailleBlockPerso,
+			Monde monde) throws SlickException {
 		super(x, y, masse, tailleBlockPerso, monde);
+		sheet = new XMLPackedSheet("res/bat.png",
+				"res/bat.xml");
 	}
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
-		// TODO Auto-generated method stub
-		image = new Image("res/ennemiVert.png");
+		image = sheet.getSprite("bat_00.png");		
 	}
 
 	public void render(Graphics g) {
+		i++;
+		if (i >= 6) {
+			animationStep++;
+			animationStep %= 3;
+			image = sheet.getSprite("bat_0" + animationStep + ".png");			
+			if (getDirectionDroite()) {
+				image = image.getFlippedCopy(true, false);
+			}
+			i = 0;
+		}
+		
 		// dessine l'image de l'ennemi en le centrant
 		image.drawCentered(getX(), getY());
 		// texte de debug
