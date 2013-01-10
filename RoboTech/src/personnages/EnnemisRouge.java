1f3e9760d4c8843d47de15722fd795756dae3adf
baptiste <baptiste.haudegand@gmail.com>
2013-01-10 16:44:28 +0100

    Sprite et animation pour ennemi rouge

--- RoboTech/src/personnages/EnnemisRouge.java
+++ RoboTech/src/personnages/EnnemisRouge.java
@@ -6,27 +6,42 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.XMLPackedSheet;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class EnnemisRouge extends Ennemis {
 	// l'image qui contient le sprite du robot
 	private Image image;
+	private XMLPackedSheet sheet;
+	private int i;
+	private int animationStep;
 
 	public EnnemisRouge(float x, float y, float masse, float tailleBlockPerso,
 			Monde monde) throws SlickException {
 		super(x, y, masse, tailleBlockPerso, monde);
 
-		// charge l'image du robot
-		image = new Image("res/ennemiRouge.png");
+		sheet = new XMLPackedSheet("res/monster.png",
+				"res/monster.xml");
 	}
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
-
+		image = sheet.getSprite("monster_00.png");
 	}
 
 	public void render(Graphics g) {
+		i++;
+		if (i >= 8) {
+			animationStep++;
+			animationStep %= 9;
+			image = sheet.getSprite("monster_0" + animationStep + ".png");			
+			if (getDirectionDroite()) {
+				image = image.getFlippedCopy(true, false);
+			}
+			i = 0;
+		}
+		
 		// dessine l'image de l'ennemi en le centrant
 		image.drawCentered(getX(), getY());
 
