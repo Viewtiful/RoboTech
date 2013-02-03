1b87d02355a43161f46f0b0152788cf84e62dddd
MOISE-Antoine <moise.antoine@hotmail.fr>
2013-02-03 16:37:56 +0100

    [Mise à jour] : Création de la BlocsBlessantFactory qui permet des créer des blocs nuisible pour le personnages.

---
+++ RoboTech/src/factory/BlocsBlessantFactory.java
@@ -0,0 +1,27 @@
+package factory;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import net.phys2d.raw.World;
+
+import org.newdawn.slick.tiled.TiledMap;
+
+import blocs.BlocsBlessant;
+
+public class BlocsBlessantFactory extends AbstractFactory{
+
+	private HashMap<String,BlocsBlessant> chaine;
+	public BlocsBlessantFactory(World world, TiledMap map)
+	{
+		super(world,map);
+	}
+	
+	private BlocsBlessant get_produit(String name) {
+		return chaine.get(name);
+	}
+
+	public Collection<BlocsBlessant> get_produit() {
+		return chaine.values();
+	}
+}
