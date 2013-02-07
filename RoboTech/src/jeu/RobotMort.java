7add8cbaa09cf200047257c471375471b0d01149
baptiste <baptiste.haudegand@gmail.com>
2013-02-07 20:20:20 +0100

    Commentaire RobotMort.java

--- RoboTech/src/jeu/RobotMort.java
+++ RoboTech/src/jeu/RobotMort.java
@@ -8,45 +8,90 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+/**
+ * Etat dans lequel rentre le joueur lorsque celui-ci meurt
+ * @author Equipe RoboTech
+ *
+ */
 public class RobotMort extends BasicGameState {
+	/**
+	 * ID de l'état
+	 */
 	private int ID = -1;
+	/**
+	 * Contient les images de l'état
+	 */
 	private Image[] itemMort;
+	/**
+	 * Permet de savoir si la souris se trouve dans un bouton
+	 */
 	private boolean[] interieurBouton;
+	/**
+	 * Permet de positionner les images de l'état
+	 */
 	private int[][] positionItemOption;
+	/**
+	 * Position en x de la souris
+	 */
 	private int sourisX;
+	/**
+	 * Position en y de la souris
+	 */
 	private int sourisY;
 
+	/**
+	 * Constructeur de Victoire
+	 * @param ID transmis
+	 */
 	public RobotMort(int ID) {
 		this.ID = ID;
 	}
 
+	/**
+	 * Retourne l'ID de l'état
+	 */
 	@Override
 	public int getID() {
 		return ID;
 	}
 
+	/**
+	 * Initialisation les images ainsi que leurs positions
+	 */
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
+		//initialise les images de l'état
 		itemMort = new Image[1];
 		itemMort[0] = new Image("res/retourOption.png");
 
+		//initialise le tableau pour savoir si la souris se trouve sur un bouton à false
 		interieurBouton = new boolean[itemMort.length];
+		
+		//initialise le tableau des positions des images
 		positionItemOption = new int[itemMort.length][2];
 	}
 
+	/**
+	 * Gére l'affichage des images de l'état
+	 */
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		// retour menu principal
+		//position les images et les affiche
 		positionItemOption[0][0] = 300;
 		positionItemOption[0][1] = 280;
 		itemMort[0].draw(positionItemOption[0][0], positionItemOption[0][1]);
 	}
 
+	/**
+	 * Gére la mise à jour de l'état, gestion de la souris avec connaissance de sa position et action à faire si celle-ci se trouve
+	 * sur un bouton
+	 */
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
+		//gestion de la souris
 		Input input = gc.getInput();
 
 		sourisX = input.getMouseX();
