c8ccbd8816eae5a80ac092328ee304380b0f6558
baptiste <baptiste.haudegand@gmail.com>
2013-02-13 21:42:28 +0100

    Commentaire serpent

--- RoboTech/src/personnages/Serpent.java
+++ RoboTech/src/personnages/Serpent.java
@@ -9,14 +9,43 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.XMLPackedSheet;
 import org.newdawn.slick.state.StateBasedGame;
 
+/**
+ * Gere les serpents
+ * @author Equipe RoboTech
+ *
+ */
 public class Serpent extends Ennemis {
-	// l'image qui contient le sprite du robot
+	/**
+	 * Image courant de l'animation du serpent
+	 */
 	private Image image;
+	/**
+	 * Sprite contenant les images de l'animation du robot
+	 */
 	private XMLPackedSheet sheet;
+	/**
+	 * Permet de compter les secondes
+	 */
 	private int i;
+	/**
+	 * Gestion de l'animation courante
+	 */
 	private int animationStep;
+	/**
+	 * Permet de savoir si le serpent a ete touche
+	 */
 	private boolean toucherDmg;
 
+	/**
+	 * Constructeur de Serpent
+	 * @param x sa position en x
+	 * @param y sa potision en y 
+	 * @param masse sa masse dans le monde physique
+	 * @param tailleBlockPerso sa taille dans le monde physique, notamment pour sa hitbox
+	 * @param monde le monde dans lequel il evolue
+	 * @param rayonDetection le rayon de detection autour de lui dans lequel il detecte le robot
+	 * @throws SlickException
+	 */
 	public Serpent(float x, float y, float masse, float tailleBlockPerso,
 			Monde monde,float rayonDetection) throws SlickException {
 		super(x, y, masse, tailleBlockPerso, monde,rayonDetection,2);
@@ -26,14 +55,21 @@ public class Serpent extends Ennemis {
 		setVie(3);
 	}
 
+	/**
+	 * Initialisation du serpent, affichage son image de base
+	 */
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		image = sheet.getSprite("monster_00.png");
 	}
 
+	/**
+	 * Gestion de l'affichage du serpent
+	 */
 	public void render(Graphics g) {
 		i++;
+		//gestion des animations du serpent
 		if (i >= 8) {
 			animationStep++;
 			animationStep %= 9;
@@ -44,6 +80,7 @@ public class Serpent extends Ennemis {
 			i = 0;
 		}
 
+		//s'il a ete touché, on le rend transparent, pour le faire clignoter
 		if (toucherDmg) {
 			image.setAlpha(0.001f);
 			if (i >= 5) {
@@ -61,10 +98,12 @@ public class Serpent extends Ennemis {
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-
 		render(g);
 	}
 
+	/**
+	 * Gestion des degats subient lorsqu'il est touche
+	 */
 	@Override
 	public void toucher(int value) {
 		toucherDmg = true;
